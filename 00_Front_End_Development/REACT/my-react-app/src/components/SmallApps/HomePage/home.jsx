c756b3aae52fb2216bcafe4bc7a59c760f689810
Allen5288 <yuanxin_xie_ee@163.com>
2025-01-29 15:05:26 +0800

    20250129-1505-addPersonalUITest

--- 00_Front_End_Development/REACT/my-react-app/src/components/SmallApps/HomePage/home.jsx
+++ 00_Front_End_Development/REACT/my-react-app/src/components/SmallApps/HomePage/home.jsx
@@ -1,10 +1,23 @@
-import { Link } from "react-router-dom";
+import { Button, Tabs } from "devlifeui-react";
 
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to my home page!</p>
+      <Button variant="primary" size="lg">
+        Click me 2
+      </Button>
+
+      <Button variant="default" size="sm">
+        Click me
+      </Button>
+      <Tabs
+        tabs={[
+          { id: "tab1", label: "Tab 1", content: "Content 1" },
+          { id: "tab2", label: "Tab 2", content: "Content 2" },
+        ]}
+      />
     </div>
   );
 }
